871c26edc61951e21c58543280b519ada2a94130
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-17 15:18:21 +0800

    Removed single file build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { viteSingleFile } from "vite-plugin-singlefile";
 import tailwindcss from "@tailwindcss/vite";
 
-const SingleFileConfig = {
-  ...viteSingleFile(),
-  enforce: "post",
-  apply: "build",
-} as PluginOption;
-
 export default defineConfig({
-  plugins: [svelte(), tailwindcss(), SingleFileConfig],
+  plugins: [svelte(), tailwindcss()],
   server: {
     port: 3000,
     host: true,
