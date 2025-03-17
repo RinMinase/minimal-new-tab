c20fe6fc02c189c0c8ade49b244e2ba907508313
Rin Minase <rin.black.raison.detre@gmail.com>
2025-03-17 11:59:56 +0800

    Removed warnings on accessibility

--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   preprocess: vitePreprocess(),
+  onwarn: (warning, handler) => {
+    // suppress warnings on `vite dev` and `vite build`
+    if (warning.code.includes("a11y")) return;
+    handler(warning);
+  },
 };
